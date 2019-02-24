6bcaabf5ab9cc3acbb25c5ae804de80f931d0713
Sumit Gupta <sumit.gupta@treebohotels.com>
2019-02-24 10:33:02 +0530

    Add Virtual list using react window

--- src/client/src/views/CategorySelector/CategorySelectorDropdown/CategorySelectorDropdown.js
+++ src/client/src/views/CategorySelector/CategorySelectorDropdown/CategorySelectorDropdown.js
@@ -1,51 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
 import Accordion from '../../../components/Accordion/Accordion';
+import List from '../../../components/List/List';
 
-const CategorySelectorDropDown = (props) => {
-  const {
-    categoriesData,
-    onSelectCategoryValue,
-    onSelectFullCategory
-  } = props;
-  const selectAll = category => e => {
+class CategorySelectorDropDown extends React.Component {
+  selectAll = (category) => (e) => {
     e.stopPropagation();
-    console.log('All selected')
     onSelectFullCategory(category);
-  };
-  return (
-    <Accordion.Container className="category-selector__categories">
-      {
-        categoriesData.map((category) => (
-          <Accordion.Section>
-            <Accordion.Title>
-              <span>{category.name}</span>
-              <span onClick={selectAll(category)} className="">
-                Select All
-              </span>
-            </Accordion.Title>
-            <Accordion.Content className="category-selector__category">
-              <ul className="category-selector__categories__list">
-                { 
-                  category.values.map((value, index) => {
-                    const key = `${category.name}-${index}`
-                    return (
-                      <li
-                        key={key}
-                        className="category-selector__categories__list__value"
-                        onClick={() => onSelectCategoryValue(category.name, value)}
-                      >
-                        {value}
-                      </li>
-                    );
-                  })
-                }
-              </ul>
-            </Accordion.Content>
-          </Accordion.Section>
-        ))
-      }
-    </Accordion.Container>
-  );
+  }
+  render() {
+    const {
+      categoriesData,
+      onSelectCategoryValue,
+      onSelectFullCategory,
+    } = this.props;
+    return (
+      <Accordion.Container className="category-selector__categories">
+        {
+          categoriesData.map((category) => (
+            <Accordion.Section>
+              <Accordion.Title>
+                <span>{category.name}</span>
+                <span onClick={this.selectAll(category)}>
+                  Select All
+                </span>
+              </Accordion.Title>
+              <Accordion.Content className="category-selector__category">
+                <List
+                  className="category-selector__categories__list"
+                  itemCount={category.values.length}
+                >
+                  {({ index, style }) => (
+                    <li
+                      className={cx("list__item", {'list__item__even': index % 2 === 0})}
+                      style={style}
+                      onClick={() => this.selectAll(category.name, category.values[index])}
+                    >
+                      {category.values[index]}
+                    </li>
+                  )}
+                </List>
+              </Accordion.Content>
+            </Accordion.Section>
+          ))
+        }
+      </Accordion.Container>
+    );
+  }
+}
+
+CategorySelectorDropDown.propTypes = {
+  categoriesData: PropTypes.object.isRequired,
+  onSelectCategoryValue: PropTypes.func.isRequired,
+  onSelectFullCategory: PropTypes.func.isRequired,
 }
 
 export default CategorySelectorDropDown;
--- src/client/src/views/CategorySelector/SelectedValues/SelectedValues.js
+++ src/client/src/views/CategorySelector/SelectedValues/SelectedValues.js
@@ -1,24 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import List from '../../../components/List/List';
 
 const SelectedValues = (props) => {
   const { selectedValues } = props;
   return (
     <div className="category-selector__selected-values">
       <div className="category-selector__selected-values__title">Selected Values</div>
-      <ul className="category-selector__selected-values__list">
-        { 
-          selectedValues && selectedValues.map((selectedValue) => (
-            <li
-              className="category-selector__selected-values__list__value"
-              key={`${selectedValue.category}-${selectedValue.value}`}
-            >
-              {selectedValue.value}
-            </li>
-          ))
-        }
-      </ul>
+      <List 
+        className="category-selector__selected-values__list"
+        itemCount={selectedValues.length}
+      >
+        {({ index, style }) => (
+          <li
+            className={cx("list__item", {
+              'list__item__even': index % 2 === 0
+            })}
+            style={style}
+          >
+            {selectedValues[index].value}
+          </li>
+        )}
+      </List>
     </div>
   )
+}
+
+SelectedValues.propTypes = {
+  selectedValues: PropTypes.array.isRequired,
 }
 
 export default SelectedValues;
