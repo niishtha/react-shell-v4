ccfd187f8bd58da1b3d78374b3e73767ff405a1a
Nishtha <niishtha@gmail.com>
2019-11-16 11:58:33 +0530

    input fai;

--- src/client/src/components/InputTest/Input.js
+++ src/client/src/components/InputTest/Input.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Input = ({value, onChangeProp}) => {
-	const onChange = e => {
-		if (onChangeProp) {
-			onChangeProp(e);
-		}
-	}
-	return <input value={value} onChange={onChange}/>;
-	// return null;
+	// const onChange = e => {
+	// 	if (onChangeProp) {
+	// 		onChangeProp(e);
+	// 	}
+	// }
+	// return <input value={value} onChange={onChange}/>;
+	return null;
 
 };
 
